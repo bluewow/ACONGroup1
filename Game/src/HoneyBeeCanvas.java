45b76e6471dcadc82d43f68d8a0eae9e42006228
egg93 <930608a@daum.net>
2019-10-01 17:09:17 +0900

    //스코어 기능 추가

--- Game/src/HoneyBeeCanvas.java
+++ Game/src/HoneyBeeCanvas.java
@@ -8,7 +8,7 @@ import java.sql.Time;
 import java.util.Timer;
 
 public class HoneyBeeCanvas extends Canvas {
-	
+
 	private BackGround bg;
 	private GameTimer t;
 	private Score s;
@@ -20,8 +20,10 @@ public class HoneyBeeCanvas extends Canvas {
 	private Bar[] bar;
 	private Butterfly bf;
 	private Flower fw;
+
+    private boolean running;
 	
-	public HoneyBeeCanvas() {
+    public HoneyBeeCanvas() {
 		bg = new BackGround();
 		bottle = new Bottle();
 		fw = new Flower();
@@ -35,19 +37,20 @@ public class HoneyBeeCanvas extends Canvas {
 		bar[1] = new Bar(70, 330, false);
 		bee = new Bee();
 		bf = new Butterfly();
+
+		running = false;
 		
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				switch (e.getKeyCode()) {
 				case KeyEvent.VK_SPACE:
-					//여기에 스페이스 누르면 명령 입력
-				}	
+					// 여기에 스페이스 누르면 명령 입력
+				}
 			}
 		});
 	}
-	
-	
+
 	@Override
 	public void update(Graphics g) {
 		paint(g);
@@ -58,49 +61,58 @@ public class HoneyBeeCanvas extends Canvas {
 		Image bufImage = createImage(this.getWidth(), this.getHeight());
 		Graphics g2 = bufImage.getGraphics();
 		bg.draw(g2, this);
-		
+
 		bottle.draw(g2, this);
-		
+
 		fw.draw(g2, this);
-		
-		for(Bar b : bar)
-			b.draw(g2,this);
-		
+
+		for (Bar b : bar)
+			b.draw(g2, this);
+
 		bee.draw(g2, this);
-		
+
 		bf.draw(g2, this);
-		
-		t.draw(g2,this);
-		
+
+		t.draw(g2, this);
+
 		s.draw(g2, this);
-		
+
 		tbee.draw(g2, this);
-		
+
 		g.drawImage(bufImage, 0, 0, this);
 	}
-	
+
 	public void start() {
-		new Thread(() ->  {
-			while (true) {
+		running = true;
+		
+		new Thread(() -> {
+			while (running) {
 				try {
 					tbee.update();
-					end();
-					
-					for(Bar b : bar)
-                		b.update();
-					
+					if (tbee.getX() == 50)
+						end();	
+					//s.update(bottle);
+					for (Bar b : bar)
+						b.update();
+
 					Thread.sleep(17);
-				} catch(InterruptedException e) {
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 				repaint();
 			}
 		}).start();
-		
+
 	}
 	
+	public void stop() {
+		running = false;
+		
+	}
+
 	public void end() {
-		if (tbee.getX()==50)
+		
 		GameFrame.getInstance().endChange();
 	}
+
 }
