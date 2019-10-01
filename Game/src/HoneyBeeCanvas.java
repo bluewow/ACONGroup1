02132814ef52b2bf69836901ebb2fd0efd451fc0
naram Kim <sylvester127@naver.com>
2019-10-01 12:24:19 +0900

    Update HoneyBeeCanvas.java

--- Game/src/HoneyBeeCanvas.java
+++ Game/src/HoneyBeeCanvas.java
@@ -1,12 +1,13 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.sql.Time;
 import java.util.Timer;
 
-public class HoneyBeeCanvas extends Canvas implements KeyListener{
+public class HoneyBeeCanvas extends Canvas {
 	
 	private BackGround bg;
 	private GameTimer t;
@@ -14,7 +15,7 @@ public class HoneyBeeCanvas extends Canvas implements KeyListener{
 	private Bee bee;
 	private TimeBee tbee;
 	private Bottle bottle;
-//	private Bar bar;
+	private Bar[] bar;
 	private Honey honey;
 	private Butterfly bf;
 	private Flower fw;
@@ -26,29 +27,23 @@ public class HoneyBeeCanvas extends Canvas implements KeyListener{
 		t = new GameTimer();
 		s = new Score();
 		tbee = new TimeBee();
-//		bar = new Bar();
+		bar = new Bar[2];
+		bar[0] = new Bar(150, 700, true);
+		bar[1] = new Bar(70, 330, false);
 		honey = new Honey();
 		bee = new Bee();
 		bf = new Butterfly();
 		
-		addKeyListener(this);
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_SPACE:
+					//여기에 스페이스 누르면 명령 입력
+				}	
+			}
+		});
 		
-	}
-
-	@Override
-	public void paint(Graphics g) {
-		Image bufImage = createImage(this.getWidth(), this.getHeight());
-		Graphics g2 = bufImage.getGraphics();
-		bg.draw(g2, this);
-		bottle.draw(g2, this);
-		fw.draw(g2, this);
-		honey.draw(g2, this);
-		bee.draw(g2, this);
-		bf.draw(g2, this);
-		t.draw(g2,this);
-		s.draw(g2, this);
-		tbee.draw(g2, this);
-		g.drawImage(bufImage, 0, 0, this);
 	}
 	
 	@Override
@@ -57,25 +52,42 @@ public class HoneyBeeCanvas extends Canvas implements KeyListener{
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_SPACE:
-			//여기에 스페이스 누르면 명령 입력
-		}	
+	public void paint(Graphics g) {
+		Image bufImage = createImage(this.getWidth(), this.getHeight());
+		Graphics g2 = bufImage.getGraphics();
+		bg.draw(g2, this);
+		
+		bottle.draw(g2, this);
+		
+		fw.draw(g2, this);
+		
+		for(Bar b : bar)
+			b.draw(g2,this);
+		
+		honey.draw(g2, this);
+		
+		bee.draw(g2, this);
+		
+		bf.draw(g2, this);
+		
+		t.draw(g2,this);
+		
+		s.draw(g2, this);
+		
+		tbee.draw(g2, this);
+		
+		g.drawImage(bufImage, 0, 0, this);
 	}
-
-	@Override
-	public void keyReleased(KeyEvent e) { }
-
-	@Override
-	public void keyTyped(KeyEvent e) { }
-
+	
 	public void start() {
 		new Thread(() ->  {
 			while (true) {
 				try {
 					tbee.update();
-
+					
+					for(Bar b : bar)
+                		b.update();
+					
 					Thread.sleep(17);
 				} catch(InterruptedException e) {
 					e.printStackTrace();
