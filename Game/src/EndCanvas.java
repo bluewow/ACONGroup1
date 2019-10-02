6129617a7c3c3f72160c27ad3d0a19f12cacf3d7
egg93 <930608a@daum.net>
2019-10-02 13:21:51 +0900

    //end캔버스 작업중

--- Game/src/EndCanvas.java
+++ Game/src/EndCanvas.java
@@ -1,12 +1,18 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class EndCanvas extends Canvas{
 	
 	@Override
 	public void paint(Graphics g) {
-		// TODO Auto-generated method stub
-		g.drawString("게임 오버", 300, 400);
+		
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Image img = tk.getImage("res/retry.png");
+		
+		g.drawImage(img,300, 400, 156, 46,this);
+		
 	}
 	
 
