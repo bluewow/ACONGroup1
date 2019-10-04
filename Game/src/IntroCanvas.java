409b443703a9521511b1b5c129f63c074161638b
egg93 <930608a@daum.net>
2019-10-04 16:41:46 +0900

    //인트로 수정

--- Game/src/IntroCanvas.java
+++ Game/src/IntroCanvas.java
@@ -1,4 +1,5 @@
 import java.awt.Canvas;
+import java.awt.Event;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -10,6 +11,8 @@ import java.awt.event.MouseListener;
 public class IntroCanvas extends Canvas implements KeyListener, MouseListener{
 	private int x;
 	private int y;
+	private int sx;
+	private int sy;
 	private int w;
 	private int h;
 	
@@ -17,33 +20,45 @@ public class IntroCanvas extends Canvas implements KeyListener, MouseListener{
 	
 	
 	public IntroCanvas() {
-		x=200;
-		y=300;
+		x=250;
+		y=400;
+		sx=0;
+		sy=0;
+		w=255;
+		h=85;
 		Toolkit tk = Toolkit.getDefaultToolkit();
-		//img = tk.getImage("res/IntroBeforeBtnTemplet(255X85).png");
+		img = tk.getImage("res/IntroBeforeBtnTemplet(255X85).png");
+		
+		addKeyListener(this);
+		addMouseListener(this);
 	}
 	
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		g.drawImage(img, 200, 300,255,85,this);
-=======
-//		g.drawImage(img, 200, 300,this);
->>>>>>> d4bc7f74bd0cc4ec501ca9825cc511820e10b48e
-		addKeyListener(this);
-		addMouseListener(this);
+		g.drawImage(img, x, y,x+w,y+h,sx,sy,w,h,this);
+		g.drawImage(img, x, y+100,x+w,y+100+h,sx+w,sy,sx+w*2,h,this);
+		g.drawImage(img, x, y+200,x+w,y+200+h,sx+w*2,sy,sx+w*3,h,this);	
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		if (e.getX()>=x&&e.getX()<=x+w && e.getY()>=y&&e.getY()<=y+h)
+			GameFrame.getInstance().change();
+		else if (e.getX()>=x&&e.getX()<=x+w && e.getY()>=y+200&&e.getY()<=y+200+h)
+			System.exit(0);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public boolean mouseMove(Event evt, int x, int y) {
 		// TODO Auto-generated method stub
+		return super.mouseMove(evt, x, y);
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	
 		
 	}
 
