55406d3d5e88e357f23e1726a4d0b45646fec2b8
naram Kim <sylvester127@naver.com>
2019-10-06 21:09:32 +0900

    배경음악 디버깅

--- Game/src/BgMusic.java
+++ Game/src/BgMusic.java
@@ -7,19 +7,16 @@ import javax.sound.sampled.Clip;
 
 public class BgMusic {
 	private static Clip clip;
-
+	
 	public static void Sound(String file, String Options) {
 		try {
 			AudioInputStream ais = AudioSystem.getAudioInputStream(
 					new BufferedInputStream(new FileInputStream(file)));
 			
-			if (clip == null) {
+			if (Options == "Loop") {
 				clip = AudioSystem.getClip();
 				clip.open(ais);
-			}
-			clip.start();
-
-			if (Options == "Loop") {
+				clip.start();
 				clip.loop(-1);
 			} else if (Options == "Stop") {
 				clip.flush();
